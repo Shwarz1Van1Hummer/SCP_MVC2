ec0ba6870d063d49812a89c1aeed10743d8442c2
Shwarz1Van1Hummer <113684065+Shwarz1Van1Hummer@users.noreply.github.com>
2023-03-12 23:32:47 +0600

    Update#3

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('create/', create_chat, name='home'),
-    path('<str:room_name>/<str:user>/', message_view, name='room'),
+    path('<str:room_name>/<str:username>/', message_view, name='room'),
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -5,53 +5,36 @@ from .models import *
 from apps.users.models import Profile
 
 
-# def view_messages(request):
-#     if request.method == 'GET':
-#         all_message = Message.objects.all()
-#         return render(
-#                 request=request,
-#                 template_name="main_work/chat.html",
-#                 context={
-#                     'all_message': all_message
-#                 }
-#             )
-#     else:
-#         message = request.POST.get('message')
-#         c_message = Message.objects.create(message=message)
-#         return redirect('/chat/message/')
-#
-
 def create_chat(request):
     if request.method == 'POST':
-        email = request.POST['email']
+        username = request.POST['username']
         room = request.POST['room']
+
         try:
             get_room = Room.objects.get(room_name=room)
-            return redirect('room', room_name=room, email=email)
+            return redirect('room', room_name=room, username=username)
 
         except Room.DoesNotExist:
             new_room = Room(room_name=room)
             new_room.save()
-            return redirect('room', room_name=room, email=email)
+            return redirect('room', room_name=room, username=username)
 
     return render(request, 'main_work/create_chat.html')
 
 
-def message_view(request, room_name, email):
-    # user = request.GET['user']
-    # image = request.FILE['image']
+def message_view(request, room_name, username):
     get_room = Room.objects.get(room_name=room_name)
 
     if request.method == 'POST':
         message = request.POST['message']
-        new_message = Message(room=get_room, sender=email, message=message)
+        new_message = Message(room=get_room, sender=username, message=message)
         new_message.save()
 
     get_message = Message.objects.filter(room=get_room)
 
     context = {
-        'messages': get_message,
-        "user": email
+        "messages": get_message,
+        "user": username
     }
     return render(
         request,
